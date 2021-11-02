92197e17326aa2fee3850f3fdad7537cd64a5550
JasperStFun <kosto55@mail.ru>
2021-11-02 12:53:20 +0300

    dynamic_page

--- newsite/news/views.py
+++ newsite/news/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 
 from .models import Article
 from .forms import ArticleForm
 
 
 def news_home(request):
-    news = Article.objects.order_by('-pub_date')
+    news = Article.objects.order_by('-pub_date')[:3]
     data = {
         'title': 'Главные новости'
     }
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/detail_views.html'
+    context_object_name = 'article'
 
 
 def create(request):
